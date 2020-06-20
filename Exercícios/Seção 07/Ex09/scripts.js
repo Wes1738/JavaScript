36310ff0b0f4d1f2e29195c8d971964d2989e672
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-06-20 11:17:17 -0300

    Seção 07 - Exercícios concluídos

---
+++ Exercícios/Seção 07/Ex09/scripts.js
@@ -0,0 +1,10 @@
+function calculaPotencia(x, y) {
+    // return Math.pow(x, y);
+    return x ** y;
+}
+
+console.log(calculaPotencia(2,3));
+console.log(calculaPotencia(3,3));
+console.log(calculaPotencia(65,2));
+console.log(calculaPotencia(10,3));
+console.log(calculaPotencia(3,2));
---
+++ Exercícios/Seção 07/Ex10/scripts.js
@@ -0,0 +1,14 @@
+function decrementaComPar(num) {
+    
+    for (let i = num; i > 0; i--) {
+        
+        if (i % 2 == 0) {
+            console.log(i);
+        }
+        
+    }
+}
+
+decrementaComPar(9);
+decrementaComPar(20);
+decrementaComPar(30);
