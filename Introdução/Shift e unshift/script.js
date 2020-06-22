67b766d53ae2487542b29355826c6844c5329d2d
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-06-22 11:17:18 -0300

    Shift e unshift

---
+++ Introdução/Shift e unshift/script.js
@@ -0,0 +1,13 @@
+// ● shift: remove o primeiro elemento de um array;
+// ● unshift : adiciona um ou mais elementos no início de um array;
+
+let nomes = ["Marcos", "Lucas", "Mateus", "Ana Clara", "Luana"];
+
+let elementoRemovido = nomes.shift();
+
+console.log(elementoRemovido);
+
+nomes.unshift("Mackenzie");
+
+console.log(nomes);
+
