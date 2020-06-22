de6aaf7e0a6aeece06a8d1f7835464ffdbde0aab
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-06-22 11:42:52 -0300

    Mia sobre Slice

--- Introdução/Slice/script.js
+++ Introdução/Slice/script.js
@@ -7,3 +7,6 @@ let nums = [0,1,2,3,4,6,5,2,9,8,7,10,12,54,23,32,24,27,17,38];
 console.log(nums.slice(6));
 console.log(nums.slice(6,7));
 console.log(nums.slice(6,10));
+console.log(nums.slice(-3)); //Pega ao inverso
+console.log(nums.slice(3, -3)); //Esse vai a partir do elemento no indice 3 mas retira os tres ultimos elementos
+
