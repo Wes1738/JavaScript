d198c4eb91b02cb569557fba9068db28f109b09f
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-06-22 11:38:19 -0300

    Slice

---
+++ Introdução/Slice/script.js
@@ -0,0 +1,9 @@
+// ● Retorna um array a partir de outro array;
+// ● O array retornado é determinado pelos parâmetros que são os
+// índices de início e fim do novo array;
+
+let nums = [0,1,2,3,4,6,5,2,9,8,7,10,12,54,23,32,24,27,17,38];
+
+console.log(nums.slice(6));
+console.log(nums.slice(6,7));
+console.log(nums.slice(6,10));
