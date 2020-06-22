3760217691f778487f302464b60287baf4fb059c
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-06-22 11:30:17 -0300

    indexOf e lastIndexOf

---
+++ Introdução/IndexOf e lastIndexOf/script.js
@@ -0,0 +1,7 @@
+// ● indexOf: encontra o índice de um determinado elemento;
+// ● lastIndexOf: encontrar o último índice de um elemento;
+
+let numeros = [0, 1, 2, 3, 1, 2];
+
+console.log(numeros.indexOf(1));
+console.log(numeros.lastIndexOf(1));
