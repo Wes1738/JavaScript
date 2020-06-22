dfebba2c0b766d9a127bbcb7bf93f8f504794dca
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-06-22 11:12:09 -0300

    Pop e Push

---
+++ Introdução/Pop e Push/script.js
@@ -0,0 +1,10 @@
+let nomes = ["Marcos", "Lucas", "Mateus", "Ana Clara", "Luzia"];
+
+let elementoRemovido = nomes.pop();
+
+console.log(elementoRemovido);
+
+nomes.push("Ariella");
+
+console.log(nomes);
+
