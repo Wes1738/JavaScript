155f336f9eb26f0dd3d9568c4f327c83f5669c0d
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-06-22 09:19:28 -0300

    Objetos

---
+++ Introdução/Objetos/script.js
@@ -0,0 +1,14 @@
+let cavalo = {
+    cor: 'Branca',
+    raca: 'Marchador',
+    idade: 12,
+    comer: function comer() {
+        console.log("Estou comendo!");
+    }
+};
+
+console.log(cavalo.cor);
+cavalo.comer();
+
+// [] = Array
+// {} = Object
